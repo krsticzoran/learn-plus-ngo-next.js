7752e4ea1c60521bae1ffc572629dac7a932d22a
zorannkrstic <zorankrstic81@gmail.com>
2025-01-15 17:49:36 +0100

    google map

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,8 @@
+import GoogleMapComponent from "@/components/ui/google-map";
 export default function Contact() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <h1 className="text-4xl font-semibold text-gray-800">Contact</h1>
-    </div>
+    <>
+      <GoogleMapComponent />
+    </>
   );
 }
