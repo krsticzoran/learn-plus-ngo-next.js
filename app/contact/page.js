6ae3d406ec11620804aadf079be4e1bbf6efebd0
zorannkrstic <zorankrstic81@gmail.com>
2025-01-18 11:26:05 +0100

    onUnmount

--- app/contact/page.js
+++ app/contact/page.js
@@ -4,21 +4,41 @@ import GoogleMapComponent from "@/components/ui/google-map";
 export default function Contact() {
   return (
     <>
-      <section className="bg-white bg-[url('/images/shape-15.png')] bg-cover bg-center bg-no-repeat ">
-        <div className="max-w-screen-xl mx-auto py-28  ">
-          <h1 className="text-4xl sm:text-6xl font-bold text-center mb-24 mt-24 capitalize text-primary">
-            Contact Us
-          </h1>
+      <section className="bg-primary bg-cover bg-center bg-no-repeat  ">
+        <div className="max-w-screen-xl mx-auto pt-28  ">
+          <div className="flex items ">
+            <div className="w-[50%] mt-20">
+              <h1 className="text-4xl sm:text-6xl font-bold   capitalize text-white ">
+                get in touch
+              </h1>
+              <p className="mt-4 text-lg leading-7 text-gray-two w-[90%]">
+                We’re here to help! Reach out to us through email or phone, and
+                check out the additional info PDF for directions and more.
+              </p>
+            </div>
+            <div className="w-[50%]">
+              <Image
+                src="/images/desk.jpg"
+                width={640}
+                height={423}
+                alt="desk"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
 
-          <div className="flex  space-x-8">
+      <section className="bg-white bg-[url('/images/shape-15.png')] bg-cover bg-center bg-no-repeat   ">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="flex  space-x-8 pt-28">
             {/* Visit Our Office*/}
             <div
-              className="bg-white shadow-md rounded-[20px] py-16 px-8  flex-1 flex flex-col  items-center "
+              className="bg-white shadow-md rounded-[20px] py-16 px-8  flex-1 flex flex-col  items-center  "
               style={{
                 boxShadow: "0px 0px 30px 1px rgba(0, 0, 0, 0.15)",
               }}
             >
-              <div className="flex items-center justify-center w-[70px] h-[70px] bg-white-one rounded-full hover:bg-secondary transition-colors duration-300 mb-6">
+              <div className="flex items-center justify-center w-[70px] h-[70px] bg-white-one rounded-full   mb-6">
                 <Image
                   src="/icons/location.png"
                   width={40}
@@ -37,12 +57,12 @@ export default function Contact() {
 
             {/* Let's Talk */}
             <div
-              className="bg-white shadow-md rounded-[20px] py-16 px-8  flex-1 flex flex-col  items-center "
+              className="bg-white shadow-md rounded-[20px] py-16 px-8  flex-1 flex flex-col  items-center  "
               style={{
                 boxShadow: "0px 0px 30px 1px rgba(0, 0, 0, 0.15)",
               }}
             >
-              <div className="flex items-center justify-center w-[70px] h-[70px] bg-white-one rounded-full hover:bg-secondary transition-colors duration-300 mb-6">
+              <div className="flex items-center justify-center w-[70px] h-[70px] bg-white-one rounded-full  mb-6">
                 <Image
                   src="/icons/mobile-phone.png"
                   width={40}
@@ -58,12 +78,12 @@ export default function Contact() {
 
             {/* E-mail Us*/}
             <div
-              className="bg-white shadow-md rounded-[20px] py-16 px-8  flex-1 flex flex-col  items-center "
+              className="bg-white shadow-md rounded-[20px] py-16 px-8  flex-1 flex flex-col  items-center  "
               style={{
                 boxShadow: "0px 0px 30px 1px rgba(0, 0, 0, 0.15)",
               }}
             >
-              <div className="flex items-center justify-center w-[70px] h-[70px] bg-white-one rounded-full hover:bg-secondary transition-colors duration-300 mb-6">
+              <div className="flex items-center justify-center w-[70px] h-[70px] bg-white-one rounded-full  mb-6">
                 <Image
                   src="/icons/email.png"
                   width={40}
@@ -77,30 +97,31 @@ export default function Contact() {
               <p className="text-gray-one text-lg">someemail@gmail.com</p>
             </div>
           </div>
-        </div>
-        <div className="container mx-auto mt-20 mb-28 px-4 sm:px-6">
-          <h2 className="text-4xl font-extrabold leading-[60px]   text-center text-primary mb-16">
-            Additional Information
-          </h2>
-          <div className="mb-16">
-            <iframe
-              src="/information.pdf"
-              className="w-full h-[70vh]  border rounded-md"
-              style={{
-                overflow: "hidden",
-                border: "1px solid #ccc",
-              }}
-              title="Information PDF"
-            ></iframe>
-          </div>
-          <div className="">
-            <a
-              href="/information.pdf"
-              download="Information.pdf"
-              className="text-white bg-primary px-12 py-3  text-lg rounded-md font-bold leading-6 transition-colors duration-300 font-spartan"
-            >
-              Download PDF
-            </a>
+
+          <div className="container mx-auto mt-28 mb-28 px-4 sm:px-6">
+            <h2 className="text-4xl font-extrabold leading-[60px]   text-center text-primary mb-16">
+              Additional Information
+            </h2>
+            <div className="mb-16">
+              <iframe
+                src="/information.pdf"
+                className="w-full h-[70vh]  border rounded-md"
+                style={{
+                  overflow: "hidden",
+                  border: "1px solid #ccc",
+                }}
+                title="Information PDF"
+              ></iframe>
+            </div>
+            <div className="">
+              <a
+                href="/information.pdf"
+                download="Information.pdf"
+                className="text-white bg-primary px-12 py-3  text-lg rounded-md font-bold leading-6 transition-colors duration-300 font-spartan"
+              >
+                Download PDF
+              </a>
+            </div>
           </div>
         </div>
       </section>
--- components/ui/google-map.js
+++ components/ui/google-map.js
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import React, { useCallback, useState } from "react";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "100%",
@@ -13,35 +13,54 @@ const center = {
 };
 
 export default function GoogleMapComponent() {
-  const [isLoaded, setIsLoaded] = useState(false);
+  const { isLoaded, loadError } = useJsApiLoader({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY, // Your API Key
+  });
+
+  const [mapInstance, setMapInstance] = useState(null);
+
+  // Callback to save the map instance
+  const onLoad = useCallback((map) => {
+    setMapInstance(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMapInstance(null);
+  }, []);
+
+  if (loadError) {
+    return (
+      <div
+        className="bg-red-100 text-red-500 text-center p-4"
+        style={{ height: "600px" }}
+      >
+        Failed to load Google Maps
+      </div>
+    );
+  }
+
+  if (!isLoaded || typeof google === "undefined") {
+    return (
+      <div
+        className="bg-white flex justify-center items-center"
+        style={{
+          height: "600px",
+        }}
+      >
+        <p className="text-gray-500">Loading Map...</p>
+      </div>
+    );
+  }
 
   return (
-    <div>
-      {/* Placeholder */}
-      {!isLoaded && (
-        <div
-          className="bg-white flex justify-center items-center"
-          style={{
-            height: "600px",
-          }}
-        >
-          <p className="text-gray-500">Loading Map...</p>
-        </div>
-      )}
-
-      {/* Google Map */}
-      <LoadScript
-        googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={12}
-          onLoad={() => setIsLoaded(true)}
-        >
-          <Marker position={center} />
-        </GoogleMap>
-      </LoadScript>
-    </div>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={12}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      <Marker position={center} />
+    </GoogleMap>
   );
 }
