1e1fac7194f4d9359988af8a9353d774a537e456
krsticzoran <zorankrstic81@gmail.com>
2025-02-10 10:30:41 +0100

    data

---
+++ data/services.js
@@ -0,0 +1,34 @@
+const items = [
+  {
+    question: "Structured Courses",
+    answer:
+      "Strictly focused, adaptable and tailor-made courses for groups of 4-8 participants can provide you exactly what you need. Check some of our options or send us your questions and needs and we will try to meet them.",
+  },
+  {
+    question: "Hosting of Individuals and Groups",
+    answer:
+      "As a strong regional centre of education, industry and entrepreneurship, Osijek brings you many options for your students&#39; mobilities (such as Erasmus+), job shadowings of school’s staff and structured courses. We can organize everything for you in our schools, faculties, kindergartens, companies, adult education centres according to your wishes. Just contact us and we will solve everything for you.",
+  },
+  {
+    question: "Job shadowing program",
+    answer:
+      "Imagine a situation that you have lost your ideas for teaching, managing, writing and doing your job properly. What do you think about the idea to observe some experts lessons, their preparation, teaching methods, working with equipment in different environments such as schools, companies, institutes? Having in mind that in Osijek there are 18 faculties, 18 high (secondary) and 23 primary schools and lots of kindergartens there are lot of great possibilities for on-the-job training that allows participants to follow and closely observe another colleagues performing their roles in his real-life situations.",
+  },
+  {
+    question: "Vocational Internships and Trainings",
+    answer:
+      "If you need great internship places for your vocational students, either in companies, public services or schools, let we find it for you. Our work is to provide you all: learning agreements, contracts with companies and schools, Europass Mobility documents etc. And we will not promise you that we can cover all areas here in Osijek - shipbuilding companies are not here :)",
+  },
+  {
+    question: "Project writing",
+    answer:
+      "No matter which kind of projects we are talking about, our expertise and a huge network of our experienced project writers are here. Erasmus+, Horizon, local, regional or national projects – we can assist you and help you in all phases.",
+  },
+  {
+    question: "Establishing Consortiums",
+    answer:
+      "With so many finished projects, mobilities, hostings and everything we do, we can guarantee you our partners in previous projects. Not all experiences are good – depending of your needs we will connect you with only reliable organizations and persons that can provide you their bests!",
+  },
+];
+
+export default items;
