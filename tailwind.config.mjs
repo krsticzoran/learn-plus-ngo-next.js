d02c074ec615e8fea6a552e01243db47145d31ee
krsticzoran <zorankrstic81@gmail.com>
2025-07-22 14:04:02 +0200

    fix(tailwind): namespace custom colors with 'custom-' prefix to avoid shadcn conflicts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const tailwindConfig = {
+module.exports = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,24 +7,37 @@ const tailwindConfig = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        primary: "#1D2133",
-        secondary: "#EC4E4F",
         dark: "#222222",
         "gray-one": "#676767",
         "white-one": "#F5F5F5",
         "gray-two": "#BCBDC4",
         "blue-one": "#2F3343",
+
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
+        primary: {
+          DEFAULT: "#1D2133",
+          foreground: "#1D2133",
         },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
+        secondary: {
+          DEFAULT: "#EC4E4F",
+          foreground: "#EC4E4F",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
         },
         muted: {
           DEFAULT: "hsl(var(--muted))",
@@ -34,23 +47,17 @@ const tailwindConfig = {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
         },
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        chart: {
-          1: "hsl(var(--chart-1))",
-          2: "hsl(var(--chart-2))",
-          3: "hsl(var(--chart-3))",
-          4: "hsl(var(--chart-4))",
-          5: "hsl(var(--chart-5))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
         },
       },
       fontFamily: {
-        spartan: ['League Spartan"', "sans-serif"],
+        spartan: ["League Spartan", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
       },
       borderRadius: {
@@ -62,5 +69,3 @@ const tailwindConfig = {
   },
   plugins: [require("tailwindcss-animate")],
 };
-
-export default tailwindConfig;
