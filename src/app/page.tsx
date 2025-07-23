8baf7b31a71b4bf9a92bc4961af12dd647f5a6f4
krsticzoran <zorankrstic81@gmail.com>
2025-07-23 16:17:57 +0200

    style: define headings font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <>
       <div className="relative min-h-screen flex justify-center items-center ">
-        <h1 className=" font-jetbrains  text-9xl uppercase leading-[1.3]">
+        <h1 className="   text-[40px] uppercase leading-[1.3]">
           kruno evo ti ga naslov
         </h1>
         <EarthAnimation />
       </div>
       <div className="min-h-screen">
-        <p className=" mt-20 text-center font-outfit text-2xl">ja sam text</p>
+        <p className=" mt-20 text-center text-2xl">ja sam text</p>
       </div>
     </>
   );
