e801f71f15a7fa04b1bb0851da0478d9dcb3177a
krsticzoran <zorankrstic81@gmail.com>
2025-07-29 11:53:56 +0200

    style: update desktop navigation layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         as="main"
         className="relative flex justify-start items-center  mt-[90px] lg:mt-[105px] gap-[10px] h-[calc(100vh-95px)] lg:h-[calc(100vh-110px)] "
       >
-        <div className="z-30  flex flex-col w-2/3 rounded-3xl bg-background  h-full">
+        <div className="z-30  flex flex-col w-2/3 rounded-4xl bg-background  h-full">
           <h1 className="text-6xl uppercase leading-[1.3] ">
             Learn Plus & the Journey to <br /> a Galaxy of Knowledge
           </h1>
@@ -21,7 +21,7 @@ export default function Home() {
           <CtaButton title="Uncover Our Purpose and Values" href="/" />
           <CtaButton title="Explore Our Membership Options" href="/" />
         </div>
-        <div className="bg-blue-muted h-full rounded-3xl w-1/3"> dfjdsf</div>
+        <div className="bg-blue-muted h-full rounded-4xl w-1/3"> dfjdsf</div>
       </Container>
       <div className="min-h-screen">
         <p className=" mt-20 text-center text-2xl">ja sam text</p>
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,27 +1,24 @@
 // components/Header.tsx
 import Link from "next/link";
 import { links } from "@/data/links";
-import { Button } from "@/components/ui/button";
+
 import { Container } from "./container";
 
 export const Header = () => {
   return (
     <header className="fixed top-0 left-0 w-full z-50 ">
-      <Container className="py-5 px-3 flex items-center justify-between mt-[10px] lg:mt-[15px] bg-background rounded-3xl h-[70px] lg:h-[80px] ">
+      <Container className="px-4 lg:px-6 flex items-center justify-between mt-[10px] lg:mt-[15px] bg-background rounded-3xl h-[70px] lg:h-[80px] ">
         {/* Logo */}
         <Link href="/" className=" font-bold tracking-wide uppercase ">
           Learn Plus
         </Link>
 
         {/* Nav */}
-        <nav className="hidden md:block ">
-          <ul className="flex space-x-8">
-            {links.map((link) => (
+        <nav className="hidden lg:block ">
+          <ul className="flex space-x-10">
+            {links.slice(1).map((link) => (
               <li key={link.href}>
-                <Link
-                  href={link.href}
-                  className="text-sm font-medium uppercase text-black-muted"
-                >
+                <Link href={link.href} className="capitalize text-black-muted">
                   {link.label}
                 </Link>
               </li>
@@ -31,9 +28,12 @@ export const Header = () => {
 
         {/* CTA Button */}
         <div>
-          <Button variant="outline" className="border-0 uppercase text-sm">
-            Call Us
-          </Button>
+          <a
+            href="tel:+385992476644"
+            className="bg-blue-muted hover:bg-blue-muted/90 text-white capitalize rounded-full cursor-pointer py-4 px-6 duration-300 "
+          >
+            Ask About Erasmus+
+          </a>
         </div>
       </Container>
     </header>
