9b0811e53098101b60a1e577025b608830fd8113
krsticzoran <zorankrstic81@gmail.com>
2025-10-03 11:52:22 +0200

    style: force Previous Activities section to start on new row

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -16,7 +16,7 @@ export async function fetchFromStrapi<T>(path: string): Promise<T[]> {
       "Content-Type": "application/json",
       Authorization: `Bearer ${STRAPI_TOKEN}`,
     },
-    //cache: "force-cache",
+    cache: "force-cache",
   });
 
   if (!res.ok) {
