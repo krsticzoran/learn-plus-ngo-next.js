815fdab14943d130e1e5e799b37754ad66006431
krsticzoran <zorankrstic81@gmail.com>
2025-10-06 13:15:43 +0200

    feat(responsive): hide third Erasmus card on tablet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,17 +16,21 @@ export default async function Home() {
         className="mb-2.5 rounded-4xl bg-white px-6 py-12"
       >
         <Container className="mb-2.5 grid grid-cols-1 gap-2.5 sm:grid-cols-2 xl:grid-cols-3">
-          {pastProjects.slice(1, 4).map((proj) => (
-            <ErasmusProjectCard
+          {pastProjects.slice(1, 4).map((proj, index) => (
+            <div
               key={proj.id}
-              id={proj.id}
-              slug={proj.slug}
-              title={proj.title}
-              coverUrl={proj.cover.url}
-              projectCode={proj.projectCode}
-              startDate={proj.startDate}
-              endDate={proj.endDate}
-            />
+              className={index === 2 ? "sm:hidden xl:block" : ""}
+            >
+              <ErasmusProjectCard
+                id={proj.id}
+                slug={proj.slug}
+                title={proj.title}
+                coverUrl={proj.cover.url}
+                projectCode={proj.projectCode}
+                startDate={proj.startDate}
+                endDate={proj.endDate}
+              />
+            </div>
           ))}
         </Container>
       </Container>
