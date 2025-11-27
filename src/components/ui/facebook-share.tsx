1f05ea7cc9ce1f514b16be7816879bddd3b71d59
krsticzoran <zorankrstic81@gmail.com>
2025-11-27 03:10:19 +0100

    f eat:share on facebook

--- src/components/ui/facebook-share.tsx
+++ src/components/ui/facebook-share.tsx
@@ -1,32 +1,19 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
 
 export default function FacebookShare() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  const [shareUrl, setShareUrl] = useState("");
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const fullUrl =
-        window.location.origin +
-        pathname +
-        (searchParams.toString() ? `?${searchParams}` : "");
-
-      setShareUrl(fullUrl);
-    }
-  }, [pathname, searchParams]);
+    setUrl(window.location.href);
+  }, []);
 
   return (
     <a
       href={
-        shareUrl
-          ? `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-              shareUrl,
-            )}`
+        url
+          ? `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`
           : "#"
       }
       target="_blank"
@@ -41,7 +28,12 @@ export default function FacebookShare() {
 
 function FacebookSVG(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox="0 0 24 24" fill="currentColor" {...props}>
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
       <path d="M22 12.07C22 6.49 17.52 2 12 2S2 6.49 2 12.07c0 5.03 3.66 9.2 8.44 9.93v-7.03H7.9v-2.9h2.54V9.74c0-2.5 1.49-3.89 3.77-3.89 1.09 0 2.24.2 2.24.2v2.47h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.44 2.9h-2.34V22c4.78-.73 8.44-4.9 8.44-9.93z" />
     </svg>
   );
